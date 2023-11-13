c9f8e9ec913bf089d00ed684fd8c328af994f43d
minsuje <minsu.jex@gmail.com>
2023-11-13 13:37:47 +0900

    made a slide system

--- myPage.js
+++ myPage.js
@@ -1,3 +1,42 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+let bestMove = 0;
+// bestSlides
+function nextSlidesBe() {
+    bestMove += 200;
+    let container = document.getElementsByClassName('bestSlides')[0];
+    container.style.right = bestMove + 'px';
+    for(let i=0; i < container.length; i++){
+        container.append(container.index[i]);
+        console.log(container.index[i]);
+    }
+}
 
 
+function prevSlidesBe() {
+    if(bestMove > 0){
+        bestMove -= 200;
+        let container = document.getElementsByClassName('bestSlides')[0];
+        container.style.right = bestMove + 'px';
+    }
+}
+
+//recentSlides
+let recentMove = 0;
+function nextSlidesRe() {
+    recentMove += 200;
+    let container = document.getElementsByClassName('recentSlides')[0];
+    container.style.right = recentMove + 'px';
+    console.log(recentMove);
+    console.log(container.style.right = recentMove + 'px');
+}
+function prevSlidesRe() {
+    if(recentMove > 0){
+        recentMove -= 200;
+        let container = document.getElementsByClassName('recentSlides')[0];
+        container.style.right = recentMove + 'px';
+    }
+}
+
+const cloneElement = () => {
+
+}
