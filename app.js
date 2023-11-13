17573452d046c3377e01a6fe38d909b459fae970
JHSasdf <sgh4760@naver.com>
2023-11-13 16:58:00 +0900

    231113 tranfer_to_desktop

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 $('#hamburger').click(function() {
     $('.hidden-menu').toggleClass('visible');
 })
+
+
+
+
+
+$('.card').click(function() {
+    $('.curtain').toggleClass('visible');
+    $('body').append(`
+    <div class="card hero-card" style="position: fixed; width: 50vw; height: 80vw; top: 50%; left: 50%; z-index: 3; transform: translate(-50%, -50%); ">
+      <img src=${this.children[0].src} class="card-img-top" alt="image-of-food">
+      
+      <div class="card-body" style="display: block;">
+        <p class="card-title">${this.children[1].children[0].textContent}</p>
+        <p class="card-text" style="white-space: pre-wrap;">${this.children[1].children[1].textContent}</p>
+      </div>
+    </div>`)
+})
+
+$('.curtain').click(function () {
+    $('.hero-card').remove();
+    $('.curtain').toggleClass('visible');  
+})
+
+function searchInputVisible() {
+  $('.search-input').toggleClass('visible')
+}
---
+++ swiper.js
@@ -0,0 +1,148 @@
+const swiper1 = new Swiper('.swiper1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
+    spaceBetween: 100,
+
+    autoplay: {delay: 2500},
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
+ 
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+
+      768: {
+        spaceBetween: 20,
+        slidesPerView: 4,
+      }
+    }
+  });
+
+  const swiper3 = new Swiper('.swiper3', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+
+      768: {
+        spaceBetween: 20,
+        slidesPerView: 4,
+      }
+    }
+  });
+
+  const swiper4 = new Swiper('.swiper4', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+
+      768: {
+        spaceBetween: 20,
+        slidesPerView: 4,
+      }
+    }
+  });
+
