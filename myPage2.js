439ecd0e092eb1ddfdc155162ff51c511271e9ce
minsuje <minsu.jex@gmail.com>
2023-11-15 09:07:29 +0900

    마이페이지 헤드, 바디 완성(연동 및 기능 제외)

--- myPage2.js
+++ myPage2.js
@@ -1,3 +1,4 @@
+// 프로필 이미지 클릭해서 변경
 function changeImg() {
     document.getElementById('inputImg').click();
 }
@@ -18,6 +19,8 @@ function previewFile() {
     }
 }
 
+// 좋아요 횟 수 증가
+
 function Good(){
     let count = document.getElementById('count')
     let val = count.innerText;
@@ -26,4 +29,71 @@ function Good(){
 
     val++
     count.innerText = val;
+}
+
+// 슬라이드
+let newMove = 0;
+function nextSlidesNew() {
+    newMove += 210;
+    let container = document.getElementsByClassName('newSlides')[0];
+    container.style.right = newMove + 'px';
+    console.log(newMove);
+    console.log(container.style.right = newMove + 'px');
+    if(newMove > 1200)
+    {
+        newMove = -210;
+    }
+}
+function prevSlidesNew() {
+    if(newMove > 0){
+        newMove -= 210;
+        let container = document.getElementsByClassName('newSlides')[0];
+        container.style.right = newMove + 'px';
+    }
+
+}
+
+// bestSlides
+
+let bestMove = 0;
+function nextSlidesBe() {
+    bestMove += 210;
+    let bestContainer = document.querySelectorAll('.bestSlides li');
+    let container = document.getElementsByClassName('bestSlides')[0];
+    container.style.right = bestMove + 'px';
+
+    if(bestMove > 1200)
+    {
+        bestMove = -210;
+    }
+}
+
+function prevSlidesBe() {
+    if(bestMove > 0){
+        bestMove -= 210;
+        let container = document.getElementsByClassName('bestSlides')[0];
+        container.style.right = bestMove + 'px';
+    }
+}
+
+//recentSlides
+let recentMove = 0;
+function nextSlidesRe() {
+    recentMove += 210;
+    let container = document.getElementsByClassName('recentSlides')[0];
+    container.style.right = recentMove + 'px';
+    console.log(recentMove);
+    console.log(container.style.right = recentMove + 'px');
+    if(recentMove > 1200)
+    {
+        recentMove = -210;
+    }
+}
+function prevSlidesRe() {
+    if(recentMove > 0){
+        recentMove -= 210;
+        let container = document.getElementsByClassName('recentSlides')[0];
+        container.style.right = recentMove + 'px';
+    }
+
 }
