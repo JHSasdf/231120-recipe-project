3d78f913709bf1e754351699c14eefe96796b3db
minsuje <minsu.jex@gmail.com>
2023-11-17 12:43:01 +0900

    공지사항 수정

--- mainHeader.js
+++ mainHeader.js
@@ -1,4 +1,4 @@
-let toggle = document.querySelector('.more');
+let toggle2 = document.querySelector('.more');
 let icon = document.querySelector('.icon')
 let menu = document.querySelector('.menu');
 let login = document.querySelector('.login');
@@ -8,7 +8,7 @@ let searchBox = document.querySelector('.searchBox')
 
 let back = document.querySelector('.back');
 
-toggle.addEventListener('click', function () {
+toggle2.addEventListener('click', function () {
     menu.classList.toggle('active');
     // login.style.display = 'none';
     search.classList.toggle('inactive');
@@ -17,10 +17,15 @@ toggle.addEventListener('click', function () {
 })
 search.addEventListener('click', function () {
     searchBox.classList.toggle('active');
+    if (window.innerWidth <= 768) {
+
+        $('body').toggleClass('overflow-hidden')
+    }
     // login.classList.toggle('inactive');
     // toggle.classList.toggle('inactive');
 })
 
 back.addEventListener('click', function () {
     searchBox.classList.remove('active');
+    $('body').removeClass('overflow-hidden')
 })
